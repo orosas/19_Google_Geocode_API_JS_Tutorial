4093ead2b613b7df17d126e53f7879f377b57776
Omar Rosas <jorosas@gmail.com>
2020-07-06 16:36:11 -0500

    Código de creción de mapa, single marker con Info window

---
+++ app.js
@@ -0,0 +1,56 @@
+// Nota: Google Maps requiere activar la cuenta de usuario para obtener la API Key
+//     y activar la cuenta para pago.API
+
+//     Existe un crédito mensual de $ 200 USD, (JULIO 2020). Los costos adicionales
+//     se calculan en la sección de Dynamic Maps:
+//     https://cloud.google.com/maps-platform/pricing/sheet/
+
+// Para crear una API Key entrar <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+// https://console.developers.google.com/
+
+// Google Maps JavaScript API v3 Reference:
+// https://developers.google.com/maps/documentation/javascript/reference
+
+// Referencia: Google Maps markers
+// https://developers.google.com/maps/documentation/javascript/markers
+
+var map;
+function initMap() {
+    
+
+    // Map options
+    let options = {
+      // Nota: Entre más altal el valor, más "cercana" es la imagen
+      //     del map o con MÁS ZOOM.
+        zoom: 14,
+        center: {lat: 19.432778, lng: -99.133333}
+    }
+
+  // Creación del Mapa
+  let map = new google.maps.Map(document.getElementById("map"), options);
+  
+  // ******* Nota: Crea marker en el mapa
+  let coordBellasArtes = {lat: 19.4356000, lng: -99.1413000};
+  
+  const imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+  let bellasArtesMarker = new google.maps.Marker({
+    position: coordBellasArtes,
+    map: map,
+    title: 'Palacio de Bellas Artes',
+    icon: imagen
+  });
+  // ***********************
+
+  // ++++++++++++++ Info window
+  // Nota: Se requiere un addEventListener
+  let infoWindow = new google.maps.InfoWindow({
+    content:'<h1>Bellas Artes</h1>'
+  });
+  bellasArtesMarker.addListener('click', function(){
+    console.log('Dentro de infoWindow');
+    infoWindow.open(map, bellasArtesMarker);
+  })
+  // ++++++++++++++++
+
+}
