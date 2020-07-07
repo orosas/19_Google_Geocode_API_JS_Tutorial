a4bee823b4144cc1f966de5a7a3fec249343be4f
Omar Rosas <jorosas@gmail.com>
2020-07-06 20:07:26 -0500

    Código para agregar multiples markers usando una función

--- app.js
+++ app.js
@@ -20,37 +20,63 @@ function initMap() {
 
     // Map options
     let options = {
-      // Nota: Entre más altal el valor, más "cercana" es la imagen
+      // Nota: Entre más grande el valor, más "cercana" es la imagen
       //     del map o con MÁS ZOOM.
-        zoom: 14,
+        zoom: 15,
         center: {lat: 19.432778, lng: -99.133333}
     }
 
   // Creación del Mapa
   let map = new google.maps.Map(document.getElementById("map"), options);
   
-  // ******* Nota: Crea marker en el mapa
-  let coordBellasArtes = {lat: 19.4356000, lng: -99.1413000};
   
-  const imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+  // // ******* Nota: Crea marker en el mapa
+  // let coordBellasArtes = {lat: 19.4356000, lng: -99.1413000};
+  
+  // const imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+  // let bellasArtesMarker = new google.maps.Marker({
+  //   position: coordBellasArtes,
+  //   map: map,
+  //   title: 'Palacio de Bellas Artes',
+  //   icon: imagen
+  // });
+  // // ***********************
+
+  // // ++++++++++++++ Info window
+  // // Nota: Se requiere un addEventListener
+  // let infoWindow = new google.maps.InfoWindow({
+  //   content:'<h1>Bellas Artes</h1>'
+  // });
+  // bellasArtesMarker.addListener('click', function(){
+  //   console.log('Dentro de infoWindow');
+  //   infoWindow.open(map, bellasArtesMarker);
+  // })
+  // // ++++++++++++++++
+
+
+
+  // >>>>> Multiple Markers
+  addMarker({lat: 19.4356000, lng: -99.1413000}); // Bellas Artes
+  addMarker({lat: 19.432778, lng: -99.133333}); // Zócalo CDMX
+  addMarker({lat: 19.431270, lng: -99.141998}); // Zócalo CDMX
+
+
+
+  // Add marker function
+  function addMarker(coords) {
+      // ******* Nota: Crea marker en el mapa
+
+      const imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 
   let bellasArtesMarker = new google.maps.Marker({
-    position: coordBellasArtes,
+    position: coords,
     map: map,
     title: 'Palacio de Bellas Artes',
     icon: imagen
   });
-  // ***********************
+  }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-  // ++++++++++++++ Info window
-  // Nota: Se requiere un addEventListener
-  let infoWindow = new google.maps.InfoWindow({
-    content:'<h1>Bellas Artes</h1>'
-  });
-  bellasArtesMarker.addListener('click', function(){
-    console.log('Dentro de infoWindow');
-    infoWindow.open(map, bellasArtesMarker);
-  })
-  // ++++++++++++++++
 
 }
